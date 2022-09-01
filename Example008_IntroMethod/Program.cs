5ce2373e8bd84cc12cb08c7fac45ee1b07adddc6
Voxdan21 <popov.daniil21@gmail.com>
2022-09-01 21:09:28 +0300

    упростил одной строкой  новую задачу

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -17,8 +17,10 @@ int a3 = 13;
 int b3 = 2333;
 int c3 = 313;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1),Max(a2, b2, c2),Max(a3, b3, c3));
 Console.WriteLine(max);
